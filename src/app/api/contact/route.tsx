5781cb44567852f07f3b41f55436cf86035faafe
Engr Munib <engr.munib07@gmail.com>
2025-08-22 14:09:10 +0500

    contactus form api working

--- src/app/api/contact/route.tsx
+++ src/app/api/contact/route.tsx
@@ -4,7 +4,9 @@ export async function POST(req: Request) {
   try {
     const formData = await req.json();
 
-    const sheetUrl ="https://script.google.com/macros/s/AKfycbwLA0GtL2BADW64DjU0wPVEW4HCwLeUuVqfIbbAiAtoCgcAG877hnqyKygT7Xd5gpFwhw/exec"
+    // logixsydev@gmail.com
+    const sheetUrl ="https://script.google.com/macros/s/AKfycbx_5mCfXB2GtvYV54_-VawFzNYm4Juq1L9XPzAP4hvJsR9wpwdVWoWluSRLIA-53AKE/exec"
+    // const sheetUrl ="https://script.google.com/macros/s/AKfycbwLA0GtL2BADW64DjU0wPVEW4HCwLeUuVqfIbbAiAtoCgcAG877hnqyKygT7Xd5gpFwhw/exec"
     const response = await fetch(sheetUrl, {
       method: 'POST',
       headers: {
@@ -16,7 +18,7 @@ export async function POST(req: Request) {
     const result = await response.json();
     return NextResponse.json(result);
   } catch (error) {
-    console.error('Proxy error:', error);
+    console.error('error in contact api:', error);
     return NextResponse.json(
         { result: 'Error', error: error instanceof Error ? error.message : String(error) },
         { status: 500 }
