1f51052334c4d71fdf973bb0e8db1dac328c972b
Engr Munib <engr.munib07@gmail.com>
2025-08-22 19:53:00 +0500

    actual sheet url added

--- src/app/api/contact/route.tsx
+++ src/app/api/contact/route.tsx
@@ -4,9 +4,10 @@ export async function POST(req: Request) {
   try {
     const formData = await req.json();
 
+    // live sheet url
+    const sheetUrl ="https://script.google.com/macros/s/AKfycbybPVFJB4lSXslnjUVfuzBNRCFWsym6nUBe2tWCOKn_BkutybEXOaFX34OCM7xmpfbggA/exec"
     // logixsydev@gmail.com
-    const sheetUrl ="https://script.google.com/macros/s/AKfycbx_5mCfXB2GtvYV54_-VawFzNYm4Juq1L9XPzAP4hvJsR9wpwdVWoWluSRLIA-53AKE/exec"
-    // const sheetUrl ="https://script.google.com/macros/s/AKfycbwLA0GtL2BADW64DjU0wPVEW4HCwLeUuVqfIbbAiAtoCgcAG877hnqyKygT7Xd5gpFwhw/exec"
+    // const sheetUrl ="https://script.google.com/macros/s/AKfycbx_5mCfXB2GtvYV54_-VawFzNYm4Juq1L9XPzAP4hvJsR9wpwdVWoWluSRLIA-53AKE/exec"
     const response = await fetch(sheetUrl, {
       method: 'POST',
       headers: {
